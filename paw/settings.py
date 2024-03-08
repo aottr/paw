9f1c7b34740864506491feddc5046c8f48a90955
Dustin Kroeger <dustin.kroeger@telekom.de>
2024-03-08 02:24:05 +0100

    📦 NEW: Add profile picture support

--- paw/settings.py
+++ paw/settings.py
@@ -138,6 +138,9 @@ STATICFILES_DIRS = (
     path.join(BASE_DIR, 'static/'),
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- paw/urls.py
+++ paw/urls.py
@@ -26,3 +26,7 @@ urlpatterns = [
     path("", include("core.urls")),
     path("", include("ticketing.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
