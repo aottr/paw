7c529211fdca2d1a05eeead2205b71e85390fe87
Alex Ottr <alex@otter.foo>
2024-04-06 22:51:51 +0200

    add fbl iteration and system user field

--- fbl_integration/admin.py
+++ fbl_integration/admin.py
@@ -3,8 +3,11 @@ from .models import FblAccount
 
 @admin.register(FblAccount)
 class FblAccountAdmin(admin.ModelAdmin):
-    list_display = ["username", "badge_number", "status", "tags_secured", "created_at"]
+    list_display = ["system_username", "username", "badge_number", "status", "tags_secured", "created_at"]
     search_fields = ["badge_number", "username"]
     list_filter = ["status"]
     readonly_fields = ["badge_number", "account_id", "username", "status", "tags_secured", "created_at"]
 
+    def system_username(self, x):
+        return x.user.username
+    system_username.short_description = 'System Username'
